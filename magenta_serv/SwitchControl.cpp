0cbfbf2b5fb149125395cce8577055d3237cc08d
tekit <tekityt@gmail.com>
2023-11-21 18:50:47 +0200

    Partly final server version

--- magenta_serv/SwitchControl.cpp
+++ magenta_serv/SwitchControl.cpp
@@ -14,37 +14,42 @@ void SwitchControl::handleClient(WiFiClient *client) {
     // Check if there is a client connected
     if (!client || !client->connected()) return;
 
-    // Read the client's request
-    String request = client->readStringUntil('\r');
-    client->flush();
-    client->println("HTTP/1.1 200 OK");
-    client->println("Content-Type: application/json");
+    String currentLine = "";
+    while (client->connected()) {
+        if (client->available()) {
+            char c = client->read();
+            Serial.write(c);
+            currentLine += c;
 
-    String response;
-    if (request.indexOf(pathOn) != -1) {
-        turnOn();
-        response = "{\"status\":\"on\"}";
-    } else if (request.indexOf(pathOff) != -1) {
-        turnOff();
-        response = "{\"status\":\"off\"}";
-    } else {
-        response = "{\"status\":\"" + "getState()" + "\"}";
+            if (currentLine.endsWith("\n")) {
+
+                String request = currentLine;
+                String response;
+
+                if (request.indexOf(pathOn) != -1) {
+                    turnOn();
+                    response = "{\"status\":\"on\"}";
+                } else if (request.indexOf(pathOff) != -1) {
+                    turnOff();
+                    response = "{\"status\":\"off\"}";
+                } else {
+                    response = "{\"status\":\""+getState()+"\"}";
+                }
+
+                // Send the HTTP response headers
+                client->println("HTTP/1.1 200 OK");
+                client->println("Content-Type: application/json");
+                client->println("Connection: close");
+                client->println("Access-Control-Allow-Origin: *");
+                client->println();
+
+                client->println(response);
+                break;
+            }
+        }
     }
-
-    // Send the HTTP response headers
-    client->println("HTTP/1.1 200 OK");
-    client->println("Content-Type: application/json");
-    client->println("Connection: close");
-    client->println("Access-Control-Allow-Origin: *");
-    client->println();
-
-    client->println(response);
-
-    Serial.println("Request received:");
-    Serial.println(request);
-    Serial.println("Response sent:");
-    Serial.println(response);
-
+    Serial.println("Request received!");
+    Serial.println("Response sent!");
     client->stop();
 }
 
